8076bf7dc334c8d5a2e540cb99662b2d32e8815b
juleswg23 <jiw1@williams.edu>
2022-07-21 14:04:16 -0400

    express

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-let port = process.env.PORT || process.argv[2] || 8081;
+let express = require("express");
+let app = express();
 
-let http = require("http");
+let port = process.env.PORT || 8081;
 
-let server = http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-
-    response.end('Hello worlddd!!\n');
-});
-
-server.listen(port, function() {
-    console.log('app up on port: ' + port);
+app.get('/', function(req, res) {
+    res.send("Hello from Express");
 })
+
+var server = app.listen(port, function () {
+    let host = server.address().address
+    let port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port);
+})
+
+
+
+// let http = require("http");
+
+// let server = http.createServer(function (request, response) {
+//     response.writeHead(200, {'Content-Type' : 'text/plain'});
+
+//     response.end('Hello world! with push\n');
+// });
+
+// server.listen(port, function() {
+//     console.log('app up on port: ' + port);
+// })
