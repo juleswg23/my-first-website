660fa3970c7945bbb1f66f600ca83138f649c7cf
juleswg23 <jiw1@williams.edu>
2022-07-25 17:09:25 -0400

    update?

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 8081;
+
+//app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
     res.send("Hello from Express");
 })
 
-const server = app.listen(p, function () {
-    let host = server.address().address
-    let p = server.address().port
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
 
-    console.log("Example app listening at http://%s:%s", host, p);
+const server = app.listen(port, function () {
+    let host = server.address().address;
+
+    console.log("Example app listening at http://%s:%s", host, port);
 })
 
 
